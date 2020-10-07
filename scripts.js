b6b494aedd14575a3d60124f0abf29f373fb5f90
Catarang <41228202+Catarang1@users.noreply.github.com>
2020-10-07 15:14:14 +0200

    progressBar impl (paddingFix)

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 var blue = '#00B5E6';
 var red = '#e6004a';
 var currentPhase = 'blue';
+var descAndBar = document.getElementById('descAndBar');
 
 function switchPhase(){
 	if (currentPhase == "blue") {
@@ -12,6 +13,8 @@ function switchToBlue() {
 	document.querySelector('#right>h2').innerHTML = "Shutdown";
 	document.documentElement.style.setProperty('--MainColor', blue);
 	document.getElementsByTagName('input')[0].readOnly = false;
+	descAndBar.style.border = '1px solid #21252b';
+	descAndBar.innerHTML = '<h3>Insert value in minutes, will run internal clock</h3>'
 	currentPhase = 'blue';
 }
 
@@ -19,19 +22,14 @@ function switchToRed() {
 	document.querySelector('#right>h2').innerHTML = "Cancel";
 	document.documentElement.style.setProperty('--MainColor', red);
 	document.getElementsByTagName('input')[0].readOnly = true;
+	descAndBar.style.border = '1px solid #979EA4';
+	descAndBar.innerHTML = '<div id="progressBar"></div>'
 	currentPhase = 'red';
 }
 
 /*
 Red Phase:
-innerhtml progress bar
-==
-
-Blue Phase:	
-innerHTML grey text
-
 triggering quit CMD at the end of animation
-grey text already in invisible div to simplify transition
-animate width of content
+animate width of #content
 each minute update input value in red phase
 */
